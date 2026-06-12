6bc27def8088dbb81d617f879b168ecfcc54e61f
BALACHANDRAKUMAR--!! <balachandarkumar6369@gmail.com>
2026-06-12 14:37:58 +0530

    unary and ternary operators

--- operators.js
+++ operators.js
@@ -15,3 +15,30 @@ console.log(c*=18)
 console.log(c/=10)
 console.log(c%=10)
 console.log(c**=10)
+
+//comparsion
+console.log("comparsion operator")
+var a=10
+var b=20
+console.log(a<=b)
+console.log(a>=b)
+
+//logical operators
+console.log("logical operators")
+console.log(a > 0 && b > 0)
+console.log(a > 0 && b < 0)
+console.log(a < 0 || b < 0)
+console.log(!(a > 0));
+
+//unary operators
+console.log("unary operators")
+var a=20
+console.log(a++);
+console.log(a--);
+console.log(++a);
+console.log(--a);
+
+//ternary operators 
+console.log("ternary operators")
+var result =(a > b )?"a is great" : "b is greater";
+console.log(result);
