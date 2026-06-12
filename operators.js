1bd8fbadb2257ef05dbe68a5a7220421c10eb9c1
BALACHANDRAKUMAR--!! <balachandarkumar6369@gmail.com>
2026-06-12 11:22:16 +0530

    assignment

---
+++ operators.js
@@ -0,0 +1,17 @@
+//arithmetic
+var a=15
+var b=10
+console.log(a-b);//subraction
+console.log(a*b);//multiplication
+console.log(a/b);//division
+console.log(a%0);//modulus
+console.log(a**b);//exponentiationn
+
+//Assignment
+console.log("Assignment operators")
+var c=40
+console.log(c=10)
+console.log(c*=18)
+console.log(c/=10)
+console.log(c%=10)
+console.log(c**=10)
